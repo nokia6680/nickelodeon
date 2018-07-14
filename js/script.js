a8112c83029840b2cbd5222d792acec6282345f5
nokia6680 <artur.ballod@gmail.com>
2018-07-14 23:10:24 +0200

    #slider-in-common

--- js/script.js
+++ js/script.js
@@ -57,3 +57,11 @@ if (overlaySearch) {
     }
   });
 }
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
