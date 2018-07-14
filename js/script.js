cab170ba329ee1640caed5b5e9dcf7cb78db8d2e
nokia6680 <artur.ballod@gmail.com>
2018-07-15 01:53:35 +0200

    #summary-1

--- js/script.js
+++ js/script.js
@@ -60,8 +60,17 @@ if (overlaySearch) {
 
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
+  initialSlide: 1,
+  spaceBetween: 180,
+  centeredSlides: true,
+  grabCursor: true,
+  loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
 });
