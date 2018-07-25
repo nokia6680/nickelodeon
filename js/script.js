ccab9d52cf6a1e2274d0721bd9b985213eb837c8
nokia6680 <artur.ballod@gmail.com>
2018-07-25 03:51:04 +0200

    #правки-ночь
    Попапы + слайдер + ребенок + кнопка + лого.
    
    Осталась стилизация выдвигающегося меню + ховер с видео

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ var swiper = new Swiper('.swiper-container', {
   initialSlide: 1,
   centeredSlides: true,
   grabCursor: true,
-  loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -22,4 +21,11 @@ var swiper = new Swiper('.swiper-container-1', {
     el: '.swiper-pagination',
     clickable: true,
   },
+});
+
+$(document).ready(function(){
+	$("div.slide-panel p.open").toggle(function(){
+		$("div.slide-panel").animate({left:'201px'},500);}, function() {
+		$("div.slide-panel").animate({left:0},500);
+	});
 });
